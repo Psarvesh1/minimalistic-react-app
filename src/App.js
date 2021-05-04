3953ab08665ff0aed32876f5b41b093284d75c92
unknown <sarveshparab602@gmail.com>
2021-05-04 21:43:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Style.css'
+import download from './download.png'
+function App() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <img src = {download}></img>
+        </div>
+    )
+}
+
+export default App
